acd2766406b74b3992cb410fdc35c716ada65316
matt <javascript.12.03.2024@gmail.com>
2024-06-28 14:18:12 +0100

    update launch.js

--- term/launch.js
+++ term/launch.js
@@ -12,7 +12,7 @@
         var {code,stdout,stderr}    = await exec(`docker images ${image} --no-trunc`);
         if(code)return console.log('error');
         if(stdout.indexOf(image)==-1){
-              await exists('nodejs-min.dockerfile');
+              await exists(`${image}.dockerfile`);
               var {code,stdout,stderr}    = await exec(`docker build . -f ${image}.dockerfile -t ${image}`);
               if(code)return console.log('error');
         }
