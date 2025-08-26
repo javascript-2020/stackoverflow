f71ef40ee5db9a8293277bded12b0d9e512f99ef
matt <javascript.12.03.2024@gmail.com>
2025-08-26 13:48:31 +0100

    Update stringify.js

--- libs/stringify.js
+++ libs/stringify.js
@@ -1,9 +1,9 @@
 
 document.write('<script src="https://cdn.jsdelivr.net/npm/string.ify/build/string.ify.min.js"></script>');
 
-function stringify(v){
+function stringify2(v){
 
-      var str   = ify(v);
+      var str   = stringify(v);
       str       = str.replace('{','{\n\n ');
       str       = str.replace(/^([^:\n]*?):/gm, '$1  :');
       str       = str.replace('}','\n\n}');
